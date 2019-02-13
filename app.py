8d2d99153b64b9654936e8bfe545f407edcf3c16
Jason Yung <json.yung@gmail.com>
2019-02-13 16:47:05 -0500

    fix type error for env var DEBUGGER_PORT

--- app.py
+++ app.py
@@ -2,13 +2,16 @@ import os
 import ptvsd
 from flask import Flask
 
-DEBUGGER_PORT = os.environ['DEBUGGER_PORT']
+DEBUGGER_PORT = int(os.environ['DEBUGGER_PORT'])
 
 try:
+    print('Starting debugger...')
     ptvsd.enable_attach(address=('0.0.0.0', DEBUGGER_PORT), secret=None)
+    print('Waiting for attach...')
     ptvsd.wait_for_attach()
 except Exception as ex:
-    print('Not working: ')
+    print('Debugger setup failed: ')
+    raise ex
 
 
 app = Flask(__name__)
